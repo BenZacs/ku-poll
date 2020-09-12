ceac9842e08153ee6018c93b1289c9e29b768f28
BenZacs <pep52414@gmail.com>
2020-09-12 12:42:49 +0700

    removed duplicated class

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,6 +22,7 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -38,37 +39,11 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
+    context = {'latest_question_list': latest_question_list, }
     return HttpResponse(template.render(context, request))
 
 
@@ -104,7 +79,7 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-    context = {'latest_question_list': latest_question_list,}
+    context = {'latest_question_list': latest_question_list, }
     return HttpResponse(template.render(context, request))
 
 
