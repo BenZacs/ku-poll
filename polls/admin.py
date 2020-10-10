df9dd044e72a0c5785a64ba1d0aa8f4e53c86276
Pep <pep52414@gmail.com>
2020-10-10 15:04:13 +0700

    add docsrtring

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-
 from .models import Choice, Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
-    ]
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}), ]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
--- polls/models.py
+++ polls/models.py
@@ -4,21 +4,26 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    """Model to creata"""
+    """Model to create question for poll."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     end_date = models.DateTimeField('ending date', default=timezone.now() + datetime.timedelta(days=10))
 
     def __str__(self):
+        """Return the question text."""
         return self.question_text
 
     def is_published(self):
+        """Check is the question already published."""
         return timezone.now() >= self.pub_date
 
     def can_vote(self):
+        """Check is the question can be voted."""
         return self.end_date > timezone.now() >= self.pub_date
 
     def was_published_recently(self):
+        """Check is the question is recently published in one day."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
@@ -28,9 +33,12 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """Create choice for the question."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """Return choice text."""
         return self.choice_text
